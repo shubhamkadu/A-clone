235b5e089e70da406b88d169afdf9e5eb60a8b7c
shubhamkadu <shubhampkadu@gmail.com>
2021-10-14 21:48:22 -0600

    amazon

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "fakestoreapi.com",
+      "pixabay.com",
+      "unsplash.com/",
+      "istockphoto.com",
+    ],
+  },
+};
